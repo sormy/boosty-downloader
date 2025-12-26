c9573762b8328c2bad1210dbb02d86831d08f271
Artem Butusov <art.sormy@gmail.com>
2025-12-25 23:37:31 -0500

    Bump version up

--- src/boosty_dl/__init__.py
+++ src/boosty_dl/__init__.py
@@ -4,7 +4,7 @@ from .core import download_channel_videos
 from .jellyfin import refresh_jellyfin_library
 from .plex import refresh_plex_library
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 __all__ = [
     "QUALITIES",
